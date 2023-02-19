cd29cb2da846701e918b12d05d8a487b6cffe21d
Silpashree <silpashree@bootlabstech.com>
2023-02-19 12:40:37 +0530

    initial

---
+++ main.tf
@@ -0,0 +1,7 @@
+resource "aws_api_gateway_rest_api" "New_API" {
+    name = var.api_name
+    endpoint_configuration {
+    types = var.endpoint_types
+  }
+
+}
---
+++ variables.tf
@@ -0,0 +1,16 @@
+variable "region" {
+    type = string
+}
+variable "api_name" {
+    type = string
+    description = "this is the name of rest api "
+}
+variable "endpoint_types" {
+    type = list(string)
+    description = "this is the endpoint of rest api "
+}
+
+variable "role_arn" {
+    description = "mPaaS AWS IAM ARN role" 
+    type = string
+} 
