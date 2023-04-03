3747895ebca7abcfc9e034bc847545c93c010553
SinghalHardik <singhal.hardik@mahindra.com>
2023-04-03 14:14:18 +0530

    added gw-method

--- main.tf
+++ main.tf
@@ -3,5 +3,17 @@ resource "aws_api_gateway_rest_api" "New_API" {
     endpoint_configuration {
     types = var.endpoint_types
   }
+}
 
+resource "aws_api_gateway_resource" "MyResource" {
+  rest_api_id = aws_api_gateway_rest_api.New_API.id
+  parent_id   = aws_api_gateway_rest_api.New_API.root_resource_id
+  path_part   = var.resource_name
+}
+
+resource "aws_api_gateway_method" "MyDemoMethod" {
+  rest_api_id   = aws_api_gateway_rest_api.New_API.id
+  resource_id   = aws_api_gateway_resource.MyResource.id
+  http_method   = var.http_method
+  authorization = var.authorization
 }
--- variables.tf
+++ variables.tf
@@ -14,3 +14,18 @@ variable "role_arn" {
     description = "mPaaS AWS IAM ARN role" 
     type = string
 } 
+
+variable "resource_name" {
+  type = string
+  description = "Refers to the resource name for API gateway method"
+}
+
+variable "http_method" {
+  type = string
+  description = "Refers to the http method for an api call (GET,POST,ANY)"
+}
+
+variable "authorization" {
+  type = string
+  description = "AUthorization method"
+}
