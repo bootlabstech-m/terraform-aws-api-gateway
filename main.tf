d391473d48f9170b3066f4172c98be1dfbc94ecc
Bhargava Naidu <bhargava@bootlabstech.com>
2025-07-07 16:19:34 +0530

    new provider and cli version 5.46.0

--- main.tf
+++ main.tf
@@ -1,10 +1,11 @@
 resource "aws_api_gateway_rest_api" "New_API" {
-    name = var.name
-    endpoint_configuration {
+  name = var.name
+  endpoint_configuration {
     types = var.endpoint_types
   }
-    lifecycle {
+  api_key_source = var.api_key_source
+
+  lifecycle {
     ignore_changes = [tags]
   }
-
 }
--- variables.tf
+++ variables.tf
@@ -6,10 +6,17 @@ variable "name" {
     description = "this is the name of rest api "
 }
 variable "endpoint_types" {
-    type = list(string)
-    description = "this is the endpoint of rest api "
+  description = "List of endpoint types: REGIONAL, EDGE, PRIVATE"
+  type        = list(string)
+  default     = ["REGIONAL"]
 }
+
 variable "role_arn" {
     description = "mPaaS AWS IAM ARN role" 
     type = string
 } 
+variable "api_key_source" {
+  description = "The source of the API key for requests. Valid values: HEADER or AUTHORIZER"
+  type        = string
+  default     = "HEADER"
+}
